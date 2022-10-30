b11f1ffd5f49878c714e2760fe86b1c1fa53c286
Vincent Gitonga <gitongavincent64@gmail.com>
2022-10-30 06:30:42 +0000

    Addded axios config

--- app/src/api.js
+++ app/src/api.js
@@ -1,32 +1,30 @@
 import axios from "axios";
 
-const baseUrl = "https://galadriel-server.vercel.app"
-
+const baseUrl = "https://galadriel-server.vercel.app";
+const config = {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+};
 export const getToken = async () => {
-    const resp = await axios.get(
-       `${baseUrl}/get-token`
-    );
+    const resp = await axios.get(`${baseUrl}/get-token`, config);
     let token = resp.data;
     return token;
 };
 
-
 export const createMeeting = async ({ token }) => {
     const data = { token };
 
-    const resp = await axios.post(
-        `${baseUrl}/create-meeting`,
-        data
-    );
+    const resp = await axios.post(`${baseUrl}/create-meeting`, data, config);
 
     return resp.data;
 };
 
 export const validateMeeting = async ({ meetingId, token }) => {
-    console.log(meetingId)
-    console.log(token)
+    console.log(meetingId);
+    console.log(token);
     let resp = await axios.get(
-        `${baseUrl}/validate-meeting/${token}/${meetingId}`
+        `${baseUrl}/validate-meeting/${token}/${meetingId}`,
+        config
     );
 
     return resp.data ? resp.data?.isValidated : false;
