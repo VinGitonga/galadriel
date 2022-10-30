fe58b0b5a638b47bc88dc478e7c6c6e9dd2f0c56
Vincent Gitonga <gitongavincent64@gmail.com>
2022-10-30 06:43:18 +0000

    added cors

--- server/index.js
+++ server/index.js
@@ -17,7 +17,14 @@ const SECRET_KEY = process.env.SECRET_KEY;
 const app = express();
 
 app.use(express.json());
-// app.use(cors());
+app.use(cors());
+
+app.use((req, res,next)=>{
+    res.setHeader('Access-Control-Allow-Origin','*')
+    res.setHeader('Access-Control-Allow-Methods','OPTIONS,GET,POST,PUT,PATCH,DELETE');
+    res.setHeader('Access-Control-Allow-Headers','Content-Type,Authorization');
+    next()
+})
 
 app.listen(5000, () => {
     console.log(`Server listening at port 5000`);
